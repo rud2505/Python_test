46ab16c4388a52f67fc8ecaeb99a501bba6a4635
rud2505 <osmanovrudolf@gmail.com>
2026-04-12 22:31:27 +0300

    Urok 9

--- tk_study/main.py
+++ tk_study/main.py
@@ -1,38 +1,45 @@
 import tkinter as tk
 import time
+import random
+
+COLORS = [('#e53935', '#fff'), ('#4a148c', '#fff'), ('#bbdefb', 'black'), ('#ff9800', 'black')]
 
 root = tk.Tk()
 root.title('Button Widget')
 root.geometry('500x300+700+300')
 
-def my_fn():
-    print('Hello')
-    if btn['width'] == 8:
-        btn['width'] = 20
-    else:
-        btn['width'] = 8
-    btn['text'] = 'Clicked' if btn['text'] == 'Press me' else 'Press me'
+def get_label():
+   #print(label.cget('text'))
+    print(label_text.get())
+
+def set_label():
+    label_text.set('Hello, world!!!!!!')
+
+def change_label_color():
+    idx = random.randint(0, len(COLORS) - 1)
+    color = COLORS[idx]
+    label['bg'] = color[0]
+    label['fg'] = color[1 ]
 
 
-def my_fn2(text):
-    print(text)
 
-def update_time():
-    btn4['text'] = 'Time:' + time.strftime('%H:%M:%S')
 
-time_now = time.strftime('%H:%M:%S')
-
-#btn=tk = 'Clicked' if btn['text'] == 'Press me' else 'Press me'
-btn = tk.Button(root, text='Press me', font="Verdana 10 bold", width=8, pady=5, padx=5, command=my_fn)
+btn = tk.Button(root, text='Get', font="Verdana 10 bold", width=12, pady=5, padx=5, command=get_label)
 btn.pack(pady=10)
 
-btn2 = tk.Button(root, text='Button 2', font="Verdana 10 bold", width=8, pady=5, padx=5, command=lambda: my_fn2('Test'))
+btn2 = tk.Button(root, text='Set', font="Verdana 10 bold", width=8, pady=5, padx=5, command=get_label)
 btn2.pack(pady=10)
 
-btn3 = tk.Button(root, text='Button 3', font="Verdana 10 bold", width=8, pady=5, padx=5, command=lambda: print('Test2'))
+btn3 = tk.Button(root, text='Change color', font="Verdana 10 bold", width=12, pady=5, padx=5, command=change_label_color)
 btn3.pack(pady=10)
 
-btn4 = tk.Button(root, text=f'Time:{time_now}', font="Verdana 10 bold", pady=5, padx=5, command=update_time)
-btn4.pack(pady=10)
+s = 'vsyciy text!!!!'
+
+label_text = tk.StringVar()
+label_text.set(s)
+
+
+label = tk.Label(root, textvariable=label_text, bg='#000', fg='#fff')
+label.pack(pady=10)
 
 root.mainloop()
