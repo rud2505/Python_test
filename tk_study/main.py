e62efa6e1f81215f3c08528db4e47f8491a247e6
rud2505 <osmanovrudolf@gmail.com>
2026-04-27 18:42:02 +0300

    Urok 9

--- tk_study/main.py
+++ tk_study/main.py
@@ -1,45 +1,125 @@
-import tkinter as tk
-import time
-import random
+#b = int(input()) #школьники
+#c = int(input()) #мандарины
 
-COLORS = [('#e53935', '#fff'), ('#4a148c', '#fff'), ('#bbdefb', 'black'), ('#ff9800', 'black')]
-
-root = tk.Tk()
-root.title('Button Widget')
-root.geometry('500x300+700+300')
-
-def get_label():
-   #print(label.cget('text'))
-    print(label_text.get())
-
-def set_label():
-    label_text.set('Hello, world!!!!!!')
-
-def change_label_color():
-    idx = random.randint(0, len(COLORS) - 1)
-    color = COLORS[idx]
-    label['bg'] = color[0]
-    label['fg'] = color[1 ]
+#a = c // b
+#d = c % b
+#print(a)
+#print(d)
 
 
+#b = int(input()) #минуты
+
+#a = b // 60 #часы
+#d = b % 60  #мин
+#print( str(b) + " " + "мин" + " " + '-' + " " + 'это' + " " + str(a) + " " + 'час' + " " + str(d) + " " + 'минут.')
+
+#-----------------------------------------------
+
+#
+# n = int(input())
+#
+# a = n // 100          # сотни
+# b = (n // 10) % 10    # десятки
+# c = n % 10            # единицы
+#
+# sum_digits = a + b + c
+# prod_digits = a * b * c
+
+# print("Сумма цифр =", sum_digits)
+# print("Произведение цифр =", prod_digits)
+#---------------------------------------
+# n = int(input())
+#
+# a = n // 100          # сотни
+# b = (n // 10) % 10    # десятки
+# c = n % 10            # единицы
+#
+# print(a, b, c, sep='')
+# print(a, c, b, sep='')
+# print(b, a, c, sep='')
+# print(b, c, a, sep='')
+# print(c, a, b, sep='')
+# print(c, b, a, sep='')
+#--------------------------
+# n = int(input())
+#
+# thousands = n // 1000           # тысячи
+# hundreds = (n // 100) % 10      # сотни
+# tens = (n // 10) % 10           # десятки
+# units = n % 10                  # единицы
+#
+# print("Цифра в позиции тысяч равна", thousands)
+# print("Цифра в позиции сотен равна", hundreds)
+# print("Цифра в позиции десятков равна", tens)
+# print("Цифра в позиции единиц равна", units)
+#----------------------------
 
 
-btn = tk.Button(root, text='Get', font="Verdana 10 bold", width=12, pady=5, padx=5, command=get_label)
-btn.pack(pady=10)
+# a = int(input())
+# b = int(input())
+# c = int(input())
+#
+# if b - a == c - b:
+#     print("YES")
+# else:
+#     print("NO")
+#----------------------------------
 
-btn2 = tk.Button(root, text='Set', font="Verdana 10 bold", width=8, pady=5, padx=5, command=get_label)
-btn2.pack(pady=10)
+# n = int(input())
+#
+# # Выделяем цифры четырёхзначного числа
+# thousands = n // 1000          # первая цифра (тысячи)
+# hundreds = (n // 100) % 10     # вторая цифра (сотни)
+# tens = (n // 10) % 10          # третья цифра (десятки)
+# units = n % 10                 # четвёртая цифра (единицы)
 
-btn3 = tk.Button(root, text='Change color', font="Verdana 10 bold", width=12, pady=5, padx=5, command=change_label_color)
-btn3.pack(pady=10)
+# # Проверяем условие: сумма первой и последней = разности второй и третьей
+# if thousands + units == hundreds - tens:
+#     print("ДА")
+# else:
+#     print("НЕТ")
+#----------------------
+# a = int(input())
+# b = int(input())
+# c = int(input())
+#
+# sum_positive = 0
+#
+# if a > 0:
+#     sum_positive = sum_positive + a
+# if b > 0:
+#     sum_positive = sum_positive + b
+# if c > 0:
+#     sum_positive = sum_positive + c
+#
+# print(sum_positive)
+#-------------------------
 
-s = 'vsyciy text!!!!'
+# age = int(input())
+#
+# if age <= 13:
+#     print("детство")
+# elif age <= 24:
+#     print("молодость")
+# elif age <= 59:
+#     print("зрелость")
+# else:
+#     print("старость")
+#-------------------------
 
-label_text = tk.StringVar()
-label_text.set(s)
-
-
-label = tk.Label(root, textvariable=label_text, bg='#000', fg='#fff')
-label.pack(pady=10)
-
-root.mainloop()
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+#
+# min_num = a
+#
+# if b < min_num:
+#     min_num = b
+# if c < min_num:
+#     min_num = c
+# if d < min_num:
+#     min_num = d
+#
+# print(min_num)
+#------------
